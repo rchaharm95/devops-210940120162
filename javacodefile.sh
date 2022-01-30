01c1acd7da14f09522061f22d7f735b6494639dd
rchaharm95 <rchaharm95@gmail.com>
2022-01-30 01:38:29 -0800

    Committing script javacodefile.sh

---
+++ javacodefile.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+/* Hello World Java Program */
+class helloworld  {
+    public static void main(String[] args) {
+        System.out.println("Hello World! by prn 210940120162");
+    }
+}
